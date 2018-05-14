16314a6c0ff8e86cea03713fcda841f6fd414460
Iwan <iwan.cole.2015@bristol.ac.uk>
2018-05-14 16:03:51 +0100

    Added /me redirection to individual user page

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var bcrypt = require("bcrypt");
 var WSS = require('ws').Server;
 var express = require('express');
 var bodyParser = require('body-parser');
+var cookieParser = require("cookie-parser");
 
 
 // Start the Express server, create DB
@@ -12,12 +13,14 @@ var db = new sql.Database("data.db");
 var app = express();
 var staticPath = path.join(__dirname, '/public');
 app.use(express.static(staticPath));
+app.use(cookieParser());
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
-var globalEmails = {};
 
+var globalEmails = {};
+var globalIDs    = {};
 
 
 /* =============================================
@@ -26,7 +29,7 @@ var globalEmails = {};
 
 ============================================== */
 var db_init = function() {
-    db.run("CREATE TABLE IF NOT EXISTS users (uid, email, name, dname, picloc, salt, passhash, token)");
+    db.run("CREATE TABLE IF NOT EXISTS users (uid, id, email, name, dname, picloc, salt, passhash, token)");
 };
 
 
@@ -35,8 +38,8 @@ var db_init = function() {
 Create a new entry for new user in USERS table
 
 ---------------------------------------------- */
-var db_new_user = function(UID, email, name, dname, picloc, salt, passHash, token) {
-    db.run("INSERT INTO users (uid, email, name, dname, picloc, salt, passhash, token) VALUES(?, ?, ?, ?, ?, ?, ?, ?)", [UID, email, name, dname, picloc, salt, passHash, token], function (err) {
+var db_new_user = function(UID, ID, email, name, dname, picloc, salt, passHash, token) {
+    db.run("INSERT INTO users (uid, id, email, name, dname, picloc, salt, passhash, token) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)", [UID, ID, email, name, dname, picloc, salt, passHash, token], function (err) {
         if (err != null) {
             console.log(err);
         }
@@ -49,14 +52,50 @@ var db_new_user = function(UID, email, name, dname, picloc, salt, passHash, toke
 Get all registered emails from USERS table
 
 ---------------------------------------------- */
-var db_get_emails = function () {
-    db.all("SELECT email FROM users", function (err, row) {
+var db_get_emails_ids = function () {
+    db.all("SELECT email as email, id as id FROM users", function (err, row) {
         if (err == null) {
             var i = 0;
             while (i < row.length) {
                 globalEmails[row[i]['email'].toLowerCase()] = 1;
+                globalIDs[row[i]['id']] = 1;
                 i++;
             }
+        }
+    });
+}
+
+
+/* ---------------------------------------------
+
+Called when someone visits /me
+Takes the cookie provided in the request header
+and attempts to match it to a user ID. When a user
+is logged in, going to /me should redirect them
+to THEIR user page, else return to home (/).
+
+---------------------------------------------- */
+var db_cookie_to_ID = function (UID, token, res) {
+    db.all("SELECT uid as UID, id as id, token as token FROM users", function (err, rows) {
+        if (err == null) {
+            console.log(rows);
+            var i = 0;
+            var found = false
+            while (i < rows.length) {
+                var item = rows[i];
+                if (UID == item['UID'] && token == item['token']) {
+                    res.redirect(303, "/" + item['id']);
+                    found = true;
+                    break;
+                }
+                i++;
+            }
+            if (!found) {
+                res.redirect(303, "/");
+            }
+        } else {
+            console.log(err);
+            res.redirect(303, "/");
         }
     });
 }
@@ -70,11 +109,12 @@ Start the web server, init the database
 app.listen(8080, function() {
     console.log('Express HTTP server on listening on port 8080');
     db.serialize(db_init);
-    db_get_emails();
+    db_get_emails_ids();
 });
 
 //setInterval(function() {
 //    console.log(globalEmails);
+//    console.log(globalIDs);
 //}, 8000);
 
 
@@ -84,6 +124,18 @@ app.listen(8080, function() {
                 Helper functions
 
 ============================================== */
+/* ---------------------------------------------
+
+Pad an int with leading zero's for a given len
+
+---------------------------------------------- */
+var misc_pad_zeros = function (num, size) {
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
+
+
 /* ---------------------------------------------
 
 Validate the data is ready to be operated on
@@ -113,28 +165,28 @@ var validate_req = function(req) {
                     i++;
                 }
             } else { return false; }
-        } 
-        
-        // Login Request
-        else if (req.type == "t_login") {  
-                 
         }
-                 
+
+        // Login Request
+        else if (req.type == "t_login") {
+
+        }
+
         // Get profile requests
         else if (req.type == "t_profile") {
-            
+
         }
-        
+
         // Update profile request
         else if (req.type == "t_update") {
-                 
+
         }
-                 
+
         // Delete account request
         else if (req.type == "t_delete") {
-            
+
         }
-        
+
         else { return false; }
     } else { return false; }
     return true;
@@ -156,19 +208,19 @@ Return True *only* if all conditions are met
 ---------------------------------------------- */
 var validate_format = function(req) {
     var error = 0;
-    
+
     if (!/^[a-zA-Z]+$/.test(req.name))   { error = 1; }
     if (req.name.length > 35)            { error = 2; }
     if (req.email.length > 254)          { error = 3; }
     // TODO Check email format *@*.*
-    if ((req.email.indexOf("@") == -1 ) || (req.email.indexOf(".") == -1)) 
+    if ((req.email.indexOf("@") == -1 ) || (req.email.indexOf(".") == -1))
                                          { error = 4; }
-    if (req.pass1.length < 8 || req.pass1.length > 20) 
+    if (req.pass1.length < 8 || req.pass1.length > 20)
                                          { error = 5; }
     if (req.pass1 !== req.pass2)         { error = 6; }
     if (!/^[a-zA-Z]+$/.test(req.dname))  { error = 7; }
     if (req.dname.length > 50)           { error = 8; }
-    
+
     return error;
 };
 
@@ -198,7 +250,7 @@ var create_hash = function(plaintext, salt) {
     return {
         salt:salt,
         hash:hash
-    };  
+    };
 };
 
 
@@ -228,14 +280,23 @@ var create_user_creds = function(email, password) {
     var passwordData = create_hash(password, salt);
     var UIDData      = create_hash(email, salt);
     var token        = create_auth_token(salt, passwordData['hash']);
-    console.log(salt);
-    console.log(passwordData['hash']);
-    console.log(UIDData['hash']);
-    console.log(token);
+    var ID           = misc_pad_zeros(Math.floor(Math.random() * 10000000) + 1, 9);
+
+    while (globalIDs[ID] != undefined) {
+        ID = misc_pad_zeros(Math.floor(Math.random() * 10000000) + 1, 9);
+    }
+    globalIDs[ID] = 1;
+
+    console.log("Salt: " + salt);
+    console.log("PassHash: " + passwordData['hash']);
+    console.log("UID: " + UIDData['hash']);
+    console.log("Token: " + token);
+    console.log("ID: " + ID);
     return {
         salt: salt,
         passHash: passwordData['hash'],
         UID: UIDData['hash'],
+        ID: ID,
         token: token
     };
 };
@@ -259,6 +320,7 @@ var create_new_user = function (req) {
         var name  = req.name[0].toUpperCase() + req.name.slice(1);
         var dname = req.dname[0].toUpperCase() + req.dname.slice(1);
         db_new_user(userCredentials['UID'],
+                    userCredentials['ID'],
                     req.email.toLowerCase(),
                     name,
                     dname,
@@ -298,11 +360,24 @@ app.get("/", function(req, res) {
 });
 
 
+app.get("/me", function (req, res) {
+    if (req.cookies['loginAuth'] != undefined) {
+        var cookie = req.cookies['loginAuth'];
+        if (cookie.length == 121) {
+            if (cookie.indexOf("|") == 60) {
+                var UID = cookie.split("|")[0];
+                var token = cookie.split("|")[1];
+                db_cookie_to_ID(UID, token, res);
+            }
+        }
+    }
+});
+
 /* ---------------------------------------------
 
 Handle requests to the Signup API
 No html page here, used by POST requests to send
-data from the signup form. Validate all data 
+data from the signup form. Validate all data
 before processing, send appropriate response.
 
 ---------------------------------------------- */
@@ -310,15 +385,15 @@ app.post("/API-signup", function(req, res) {
     console.log(req.body);
     var status = 0;
     var payload = {success:false, cookie:"", info:""};
-    
+
     /* Validate structre, types & existence of incoming request */
     if (validate_req(req.body)) {
         status = 200;
         var formatStatus = validate_format(req.body);
-        
+
         if (formatStatus == 0) {
-            payload = create_new_user(req.body);            
-            
+            payload = create_new_user(req.body);
+
         } else {
             var info = "";
             switch (formatStatus) {
@@ -352,20 +427,23 @@ app.post("/API-signup", function(req, res) {
             }
             payload['info'] = info;
         }
-        
+
     } else {
         status = 400;
         payload['info'] = "Bad request formatting >:(";
     }
     res.status(status)
-    
+    res.cookie("loginAuth", payload['cookie'],
+               { expires: new Date(Date.now() + 90000000000),
+                 encode: String});
+
     // This is purposefully slowing down the server response time, NEEDS to
-    // be removed as the site scales up. This is done currently to show off 
+    // be removed as the site scales up. This is done currently to show off
     // the nice animation effect on the client side.
     setTimeout(function () {
         res.send(payload);
     }, 1500);
-    
+
 });
 
 
@@ -399,4 +477,3 @@ app.delete("*", function (req, res) {
     res.status(403);
     res.send("Not authorized to DELETE");
 });
-
