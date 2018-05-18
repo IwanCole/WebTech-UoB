4d39916d9fc962c640fd13956eb4dd640595ceae
Iwan <iwan.cole.2015@bristol.ac.uk>
2018-05-18 14:48:46 +0100

    Script.js passes (almost) Lint

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -19,9 +19,9 @@ var create_popup = function (text, status) {
     $(".status-popup-text").text(text);
     if (status === 0) {
         $(".status-popup-type").css("background-color", "#00d400");
-    } else if (status == 1) {
+    } else if (status === 1) {
         $(".status-popup-type").css("background-color", "#FBB13C");
-    } else if (status == 2) {
+    } else if (status === 2) {
         $(".status-popup-type").css("background-color", "#f50000");
     }
     $(".status-popup").addClass("status-popup-active");
@@ -44,84 +44,69 @@ var create_cover = function (text) {
 
 
 var post_signup = function () {
-    var inputs = $(".signup-input");
-    var payload = {
-        type:  "t_signup",
-        name:  inputs[0].value,
-        email: inputs[1].value,
-        pass1: inputs[2].value,
-        pass2: inputs[3].value,
-        dname: inputs[4].value
-    };
+    var inputs = $(".signup-input"),
+        payload = {
+            type:  "t_signup",
+            name:  inputs[0].value,
+            email: inputs[1].value,
+            pass1: inputs[2].value,
+            pass2: inputs[3].value,
+            dname: inputs[4].value
+        };
 
     $(".home-signup-btn").addClass("home-signup-btn-hide");
     $(".signup").addClass("signup-hide anim-signup-spin");
     $.post("/API-signup", payload)
            .done(function (data, staus) {
-                console.log(data);
-                if (data['success']) {
-                    create_popup(data['info'], 0);
-                    window.location.replace("/dashboard");
-                } else {
-                    create_cover(data['info']);
-                    $(".home-signup-btn").removeClass("home-signup-btn-hide");
-                    $(".signup").removeClass("signup-hide anim-signup-spin");
-                }
-            })
-           .fail(function (xhr, error, statusCode) {
-                create_popup((xhr.status.toString() + " " + statusCode), 2);
-                console.log("xhr: " + xhr + "\nError: " + error + "\nStatus: " + statusCode);
+            console.log(data);
+            if (data['success']) {
+                create_popup(data['info'], 0);
+                window.location.replace("/dashboard");
+            } else {
+                create_cover(data['info']);
                 $(".home-signup-btn").removeClass("home-signup-btn-hide");
                 $(".signup").removeClass("signup-hide anim-signup-spin");
+            }
+            })
+           .fail(function (xhr, error, statusCode) {
+            create_popup((xhr.status.toString() + " " + statusCode), 2);
+            console.log("xhr: " + xhr + "\nError: " + error + "\nStatus: " + statusCode);
+            $(".home-signup-btn").removeClass("home-signup-btn-hide");
+            $(".signup").removeClass("signup-hide anim-signup-spin");
             });
 };
 
 
 var post_login = function () {
-    var inputs = $(".login-input");
-    var payload = {
-        type:  "t_login",
-        email: inputs[0].value,
-        pass1: inputs[1].value
-    };
+    var inputs = $(".login-input"),
+        payload = {
+            type:  "t_login",
+            email: inputs[0].value,
+            pass1: inputs[1].value
+        };
 
     $(".home-login").removeClass("home-login-active");
     $.post("/API-login", payload)
            .done(function (data, staus) {
-                console.log(data);
-                if (data['success']) {
-                    window.location.replace("/dashboard");
-                } else {
-                    create_cover(data['info']);
-                    $(".home-login").addClass("home-login-active");
-                }
+            console.log(data);
+            if (data['success']) {
+                window.location.replace("/dashboard");
+            } else {
+                create_cover(data['info']);
+                $(".home-login").addClass("home-login-active");
+            }
             })
            .fail(function (xhr, error, statusCode) {
-                create_popup((xhr.status.toString() + " " + statusCode), 2);
-                console.log("xhr: " + xhr + "\nError: " + error + "\nStatus: " + statusCode);
-                $(".home-login").addClass("home-login-active");
+            create_popup((xhr.status.toString() + " " + statusCode), 2);
+            console.log("xhr: " + xhr + "\nError: " + error + "\nStatus: " + statusCode);
+            $(".home-login").addClass("home-login-active");
             });
 };
 
-//var post_login_cookie = function () {
-//    $.post("/API-login-cookie", 1)
-//           .done(function (data, staus) {
-//                console.log(data);
-//                if (data['success']) {
-//                    window.location.replace("/me");
-//                }  
-//            })
-//           .fail(function (xhr, error, statusCode) {
-//                Cookies.remove("loginAuth");
-//                Cookies.remove("session");
-//                window.location.reload();
-//            });
-//}
-
 
 var handler_signup = function () {
-    var count = 0;
-    var filled = false;
+    var count = 0,
+        filled = false;
 
     $(".home-signup-btn").click(function () {
 
@@ -139,7 +124,7 @@ var handler_signup = function () {
     
     $(".signup-input").keydown(function (key) {
         if (filled) {
-            if(key.keyCode == 13) {
+            if (key.keyCode === 13) {
                 post_signup();
             }
         }
@@ -154,9 +139,12 @@ var handler_signup = function () {
             }
         });
         var alpha = count * 0.2;
-        if (count == 5) { filled = true; }
-        else { filled = false; }
-        if (Cookies.get("theme") == 2) {
+        if (count === 5) {
+            filled = true;
+        } else {
+            filled = false;
+        }
+        if (Cookies.get("theme") === "2") {
             $("body").css("background-color", "rgba(48, 87, 196, " + alpha.toString() + ")");
         } else {
             $("body").css("background-color", "rgba(216, 17, 89, " + alpha.toString() + ")");
@@ -171,15 +159,12 @@ var handler_login = function () {
         $(".home-signup").css("overflow", "inherit");
         $(".home-welcome").addClass("home-welcome-hidden");
         
-        if (Cookies.get("theme") == 2) {
+        if (Cookies.get("theme") === "2") {
             $("body").css("background-color", "rgb(48, 87, 196)");
         } else {
             $("body").css("background-color", "rgb(216, 17, 89)");
         }
         
-//        if (Cookies.get("loginAuth") != undefined) {
-//            post_login_cookie();
-//        } else {
         $(".home-login").addClass("home-login-active");
         $(".home-signup-btn").addClass("home-signup-btn-hide");
         $(".signup").addClass("signup-hide anim-signup-spin");
@@ -190,7 +175,7 @@ var handler_login = function () {
         post_login();
     });
     $(".login-input").keydown(function (key) {
-        if (key.keyCode == 13) {
+        if (key.keyCode === 13) {
             post_login();
         }
     });
@@ -198,29 +183,32 @@ var handler_login = function () {
 
 
 var apply_theme = function (theme) {
-    if (theme == 1) {
-        var old  = "theme2";
-        var next = "theme1";
+    var old, next;
+    if (theme === 1) {
+        old  = "theme2";
+        next = "theme1";
     } else {
-        var old  = "theme1";
-        var next = "theme2";
+        old  = "theme1";
+        next = "theme2";
     }
     $(".home-signup-btn, .title, .sub-title, .sub-text, .divider, .home-login-btn, .signup, .login, footer, .chat-main-compose, .chat-main-input, .chat-main-send, .chat-message-name, .chat-message-icon, .chat-message-text, .chat-bubble-me, .nav-item-icon-cont, .nav-item-text, .profile-name, .profile-dname, .profile-delete").removeClass(old).addClass(next);
     var url = window.location.href.toString();
-    if (url.indexOf("dashboard") != -1 || url.indexOf("profile") != -1) {
+    if (url.indexOf("dashboard") !== -1 || url.indexOf("profile") !== -1) {
         $("body").removeClass(old).addClass(next);
+    } else if (url.indexOf("about") !== -1) {
+        $("html").removeClass(old).addClass(next);
     }
 };
 
 
 var handler_theme_apply = function () {
     var col = Cookies.get("theme");
-    if (col != undefined) {
+    if (col !== undefined) {
         console.log(col);
-        if (col == 1) {
+        if (col === "1") {
             theme = "theme1";
             apply_theme(1);
-        } else if (col == 2) {
+        } else if (col === "2") {
             theme = "theme2 ";
             apply_theme(2);
         }
@@ -233,8 +221,7 @@ var handler_theme_apply = function () {
     
 
 
-
-var handler_theme_switch = function () {    
+var handler_theme_switch = function () {
     $(".material-icons").click(function () {
         toggle_blackout(true);
         $(".colour-picker").fadeIn(400);
